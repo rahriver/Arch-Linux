9f2c56a1d2c70f675c9c121acb2657b0fb13b0fd
Ramin Yousefpour <ramin.y.shahrivar@gmail.com>
2022-09-19 00:22:48 +0430

    Add files via upload

--- Dotfiles/glava/bars.glsl
+++ Dotfiles/glava/bars.glsl
@@ -9,7 +9,7 @@
 /* Outline width (in pixels, set to 0 to disable outline drawing) */
 #define BAR_OUTLINE_WIDTH 0
 /* Amplify magnitude of the results each bar displays */
-#define AMPLIFY 350
+#define AMPLIFY 400
 /* Whether the current settings use the alpha channel;
    enabling this is required for alpha to function
    correctly on X11 with `"native"` transparency. */
@@ -20,7 +20,7 @@
 /* Bar color changes with height */
 #define GRADIENT (d / GRADIENT_POWER + 1)
 /* Bar color */
-#define COLOR (#00bbff * GRADIENT * ALPHA )
+#define COLOR (#0769fc* GRADIENT * ALPHA )
 /* Direction that the bars are facing, 0 for inward, 1 for outward */
 #define DIRECTION 0
 /* Whether to switch left/right audio buffers */
--- Dotfiles/glava/circle.glsl
+++ Dotfiles/glava/circle.glsl
@@ -1,11 +1,9 @@
 /* center radius (pixels) */
-#define C_RADIUS 128
+#define C_RADIUS 200
 /* center line thickness (pixels) */
-#define C_LINE 1.5
-/* outline color */
-#define OUTLINE #333333
+#define C_LINE 3.0
 /* Amplify magnitude of the results each bar displays */
-#define AMPLIFY 150
+#define AMPLIFY 300
 /* Angle (in radians) for how much to rotate the visualizer */
 #define ROTATE (PI / 2)
 /* Whether to switch left/right audio buffers */
@@ -18,7 +16,9 @@
 #define C_SMOOTH 1
 
 /* Gravity step, overrude frin `smooth_parameters.glsl` */
-#request setgravitystep 6.0
+#request setgravitystep 9.0
 
 /* Smoothing factor, override from `smooth_parameters.glsl` */
-#request setsmoothfactor 0.01
+#request setsmoothfactor 0.015
+
+#define OUTLINE #1094c9
